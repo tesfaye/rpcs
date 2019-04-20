09953d25dd0f1eb90d32a9f08b591452176a5396
akshhack <akshatp@andrew.cmu.edu>
2019-04-20 19:07:53 -0400

    Added Network API

---
+++ Wearable/src/main/java/com/example/android/google/wearable/app/Setup.java
@@ -0,0 +1,31 @@
+package com.example.android.google.wearable.app;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class Setup extends Application {
+
+    private static Setup sInstance;
+
+    private RequestQueue mRequestQueue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        sInstance = this;
+    }
+
+    public synchronized static Setup getInstance() {
+        return sInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        return mRequestQueue;
+    }
+
+}
